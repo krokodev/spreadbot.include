46cdf2223bca9f673311dcb04de264fd1d0e853d
Roman Koshelev <romakosh@gmail.com>
2015-04-16 16:45:35 +0300

    init

---
+++ gg.sh
@@ -0,0 +1,11 @@
+echo.
+echo [Add]
+git add --all
+
+echo.
+echo [Commit]
+git commit -a -m "$1"
+
+echo.
+echo [Push]
+git push
---
+++ sources/Spreadbot.Include/Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿// Crocodev (c) 2015 Crocodev
+// Crocodev.Common
+// AssemblyInfo.cs
+// romak_000, 2015-03-19 15:29
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Spreadbot.Include")]
+[assembly: AssemblyDescription("Third-party sources for the Spreadbot project")]
+[assembly: AssemblyConfiguration("release")]
+[assembly: AssemblyCompany("Krokodev")]
+[assembly: AssemblyProduct("Spreadbot.Include")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("0a5feeaa-9cf2-4670-b1c4-977d81987f92")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.1.7.*")]
+[assembly: AssemblyFileVersion("1.1")]
