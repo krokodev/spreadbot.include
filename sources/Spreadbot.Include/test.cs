d7dd89f13b239accb25f9f38b5e8f1896d845ce2
Roman Koshelev <romakosh@gmail.com>
2015-05-08 12:38:47 +0300

    added amazon product

---
+++ sources/Spreadbot.Include/test.cs
@@ -0,0 +1,33 @@
+﻿// Spreadbot.Include (c) 2015 Krokodev
+// Spreadbot.Include
+// Test.cs
+
+namespace Spreadbot.Include
+{
+    public class Test
+    {
+        private void Product()
+        {
+            /*            var request = new GetMatchingProductForIdRequest();
+
+            //'@TODO: set request parameters here
+            request.IdList = new IdListType();
+
+            // param IdType
+            request.IdType = "UPC";
+            request.IdList.Id = new List< string >();
+
+            // add the UPC to the list
+            request.IdList.Id.Add( "123456789012" );
+            request.SellerId = "merchantId";
+            request.MarketplaceId = "marketplaceId";
+
+            request.SellerId = "merchantId";
+
+             var config = new MarketplaceWebServiceProductsConfig();
+            var  client = new MarketplaceWebServiceProductsClient("appName", "appVersion", "accessKey", "secretKey", config);
+            var response = client.GetMatchingProductForId( request );
+            response.WithGetMatchingProductForIdResult(  )*/
+        }
+    }
+}
